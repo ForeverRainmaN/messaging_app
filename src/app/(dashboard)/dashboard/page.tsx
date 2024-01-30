f399890052206ed71728ef53a1162639e84c0963
Ioann Kurchin <lvRainman@gmail.com>
2024-01-31 01:45:29 +0300

    Partially created chat page, added types and validators

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,5 @@
-import Button from "@/components/ui/Button"
-import { authOptions } from "@/lib/auth"
-import { getServerSession } from "next-auth"
-
 const Page = async ({}) => {
-  const session = await getServerSession(authOptions)
-
-  return <pre>{JSON.stringify(session)}</pre>
-
-  return <Button>Hello</Button>
+  return <div>Dashboard</div>
 }
 
 export default Page

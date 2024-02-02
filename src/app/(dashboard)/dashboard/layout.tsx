c0fe6d55ae410db7d5d1ac42893961e9c8d88384
Ioann Kurchin <lvRainman@gmail.com>
2024-02-02 04:25:42 +0300

    Created mobile menu and fixed bug with friend not appearing in chatlist after accepting friend request

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOptions"
-import { Icon, Icons } from "@/components/Icons"
+import { Icons } from "@/components/Icons"
+import MobileChatLayout from "@/components/MobileChatLayout"
 import SidebarChatList from "@/components/SidebarChatList"
 import SignOutButton from "@/components/SignOutButton"
 import { getFriendsByUserId } from "@/helpers/getFriendsByUserId"
@@ -13,13 +14,6 @@ import { ReactNode } from "react"
 
 interface LayoutProps {
   children: ReactNode
-}
-
-type SidebarOption = {
-  id: number
-  name: string
-  href: string
-  Icon: Icon
 }
 
 const sidebarOptions: SidebarOption[] = [
@@ -46,7 +40,15 @@ const Layout = async ({ children }: LayoutProps) => {
 
   return (
     <div className="w-full flex h-screen">
-      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+      <div className="md:hidden">
+        <MobileChatLayout
+          friends={friends}
+          session={session}
+          sidebarOptions={sidebarOptions}
+          unseenRequestCount={unseenRequestCount}
+        />
+      </div>
+      <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link
           href="/dashboard"
           className="flex h-16 shrink-0 items-center">
